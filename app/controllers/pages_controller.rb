97b811e2adf3a43ea95fbbc49da26fae49a33055
root <lperegri@uccs.edu>
2021-05-07 18:44:54 +0000

    Iter 6

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+#this file was created for iteration 6
+
+class PagesController < ApplicationController
+    def about
+    end 
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'about' =>'pages#about', as: :about 
+  get 'contact' =>'pagescontact#', as: :contact 
+
+
   devise_for :users
   root to: 'posts#index'
   resources :posts
